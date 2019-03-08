a33fbb34ed1dca23029020feb40acf5cdde294b0
Umar Khan <umarkhandev@gmail.com>
2019-03-08 17:49:28 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+def dictionary
+  to_replace = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&",
+  }
+end
+
+def word_substituter(str)
+  str = str.split(" ")
+  str.each do |words|
+  dictionary.each do |k, v|
+      if words == k
+        words.replace(v)
+        end
+      end
+    end
+  str.join(" ")
+end
+
+def bulk_tweet_shortener(str)
+  str.each do |words|
+  dictionary.each do |k, v|
+      if words == k
+        words.replace(v)
+        end
+      end
+    end
+  puts str.join(" ")
+end
