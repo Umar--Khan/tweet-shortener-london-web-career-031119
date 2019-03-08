696d34a98f4f4b8ec461bc7c76cd3f87ddb5d629
Umar Khan <umarkhandev@gmail.com>
2019-03-08 20:22:05 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -37,7 +37,7 @@ end
 def selective_tweet_shortener(str)
   if str.join.length >= 140
     str.each do |words|
-    puts word_substituter(words)
+    puts bulk_tweet_shortener(str
       end
     end
   end
